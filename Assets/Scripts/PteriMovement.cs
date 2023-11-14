bb693e8544105e755beaedb44b6c7eb28ad9da21
Ktag4g <64621918+Ktag4g@users.noreply.github.com>
2023-11-14 12:23:07 -0500

    Pteri Movement Done
    -Finished Pteri movement
    --Glide does not function, need to fix
    
    Still need to do:
    -Fix Pteri Glide
    -Pteri Drop
      - Drop Cooldown
    
    - Fish Movement
    - Dash
    - Tired
    
    - Fish Death
    
    - Title screen
    - Game Over - fish
    - Game Over - pteri
    
    - Game Timer
    
    - Name for fish (IMPORTANT)

--- Assets/Scripts/PteriMovement.cs
+++ Assets/Scripts/PteriMovement.cs
@@ -7,21 +7,31 @@ public class PteriMovement : MonoBehaviour
     //Game board boundaries
     int BOARDBOUNDS = 10;
 
-    float speed = 10;
+    Vector3 ogSpot;
+    Vector3 newSpot;
 
-    /*Player Position
+    bool isGliding = false;
+
+    float speed = 2.0f;
+
+    //Player Position
     float playerHeight = -2.5f;
-    int x = 10;
-    int y = 10; */
+    int x;
+    int y;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        /*Randomizes player start position
-        x = Random.Range(-BOARDBOUNDS, BOARDBOUNDS + 1);
-        y = Random.Range(-BOARDBOUNDS, BOARDBOUNDS + 1);
-        gameObject.transform.position = new Vector3(x, y, playerHeight);*/
+        //Randomizes player start position
+        do
+        {
+            x = Random.Range(-BOARDBOUNDS, BOARDBOUNDS + 1);
+            y = Random.Range(-BOARDBOUNDS, BOARDBOUNDS + 1);
+        }
+        while (IsMovable(new Vector3(x, y, playerHeight)) == false);
+
+        gameObject.transform.position = new Vector3(x, y, playerHeight);
     }
 
     // Update is called once per frame
@@ -30,59 +40,123 @@ public class PteriMovement : MonoBehaviour
         //Move Left on Grid
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (gameObject.transform.position.x > -BOARDBOUNDS)
+            if (IsMovable(transform.position + Vector3.left * 2) == true)
             {
-                if (gameObject.transform.position.y == BOARDBOUNDS || gameObject.transform.position.y == -BOARDBOUNDS)
-                {
-                    gameObject.transform.position = gameObject.transform.position + Vector3.left;
-                }
-            }
+                transform.position = transform.position + Vector3.left * 2;
+            }            
         }
 
         //Move Right on Grid
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if (gameObject.transform.position.x < BOARDBOUNDS)
+            if (IsMovable(transform.position + Vector3.right * 2) == true)
             {
-                if (gameObject.transform.position.y == BOARDBOUNDS || gameObject.transform.position.y == -BOARDBOUNDS)
-                {
-                    gameObject.transform.position = gameObject.transform.position + Vector3.right;
-                }
+                transform.position = transform.position + Vector3.right * 2;
             }
         }
 
         //Move Up on Grid
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (gameObject.transform.position.y < BOARDBOUNDS)
+            if (IsMovable(transform.position + Vector3.up * 2) == true)
             {
-                if (gameObject.transform.position.x == BOARDBOUNDS || gameObject.transform.position.x == -BOARDBOUNDS)
-                {
-                    gameObject.transform.position = gameObject.transform.position + Vector3.up;
-                }
+                transform.position = transform.position + Vector3.up * 2;
             }
         }
 
         //Move Down on Grid
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (gameObject.transform.position.y > -BOARDBOUNDS)
+            if (IsMovable(transform.position + Vector3.down * 2) == true)
             {
-                if (gameObject.transform.position.x == BOARDBOUNDS || gameObject.transform.position.x == -BOARDBOUNDS)
-                {
-                    gameObject.transform.position = gameObject.transform.position + Vector3.down;
-                }
+                transform.position = transform.position + Vector3.down * 2;
             }
         }
 
         if (Input.GetKeyDown(KeyCode.Space /*change for arcade*/))
         {
-            Glide();
+            ogSpot = transform.position;
+            isGliding = true;
+        }
+
+        if (isGliding)
+        {
+            if (transform.position.y != BOARDBOUNDS && transform.position.y != -BOARDBOUNDS)
+            {
+                newSpot = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+
+                Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+            }
+            if (transform.position.x != BOARDBOUNDS && transform.position.x != -BOARDBOUNDS)
+            {
+                newSpot = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+                Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+            }
+
+            if (transform.position == newSpot)
+            {
+                isGliding = false;
+            }
         }
     }
 
     void Glide()
     {
+        Vector3 ogSpot = transform.position;
+        Vector3 newSpot;
 
+        if (transform.position.y != BOARDBOUNDS && transform.position.y != -BOARDBOUNDS)
+        {
+            newSpot = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+
+            Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+        }
+        if (transform.position.x != BOARDBOUNDS && transform.position.x != -BOARDBOUNDS)
+        {
+            newSpot = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+            Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+        }
+    }
+
+    void Drop()
+    {
+
+    }
+
+    bool IsMovable(Vector3 gridSpot)
+    {
+        if(gridSpot.x % 2 == 0 && gridSpot.y % 2 == 0)
+        {
+            if (gridSpot.x == BOARDBOUNDS || gridSpot.x == -BOARDBOUNDS)
+            {
+                if ((gridSpot.y <= BOARDBOUNDS && gridSpot.y >= -BOARDBOUNDS))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (gridSpot.y == BOARDBOUNDS || gridSpot.y == -BOARDBOUNDS)
+            {
+                if ((gridSpot.x <= BOARDBOUNDS && gridSpot.x >= -BOARDBOUNDS))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }        
+        }
+        else
+        {
+            return false;
+        }
     }
 }
