4f244d71e322040e0d95d4eec0cc140dce46a90e
Ktag4g <64621918+Ktag4g@users.noreply.github.com>
2023-11-16 22:40:42 -0500

    Fixed Glide/Started Drop
    - Fixed Pteri Glide
    - Started Drop

--- Assets/Scripts/PteriMovement.cs
+++ Assets/Scripts/PteriMovement.cs
@@ -7,12 +7,11 @@ public class PteriMovement : MonoBehaviour
     //Game board boundaries
     int BOARDBOUNDS = 10;
 
+    //Gliding Variables
     Vector3 ogSpot;
     Vector3 newSpot;
-
     bool isGliding = false;
-
-    float speed = 2.0f;
+    float speed = 20.0f;
 
     //Player Position
     float playerHeight = -2.5f;
@@ -30,7 +29,6 @@ public class PteriMovement : MonoBehaviour
             y = Random.Range(-BOARDBOUNDS, BOARDBOUNDS + 1);
         }
         while (IsMovable(new Vector3(x, y, playerHeight)) == false);
-
         gameObject.transform.position = new Vector3(x, y, playerHeight);
     }
 
@@ -73,54 +71,87 @@ public class PteriMovement : MonoBehaviour
             }
         }
 
+        //Starts Glide
         if (Input.GetKeyDown(KeyCode.Space /*change for arcade*/))
         {
             ogSpot = transform.position;
             isGliding = true;
+            Debug.Log("GLIDING: Current Vector is: " + ogSpot);
         }
-
         if (isGliding)
         {
-            if (transform.position.y != BOARDBOUNDS && transform.position.y != -BOARDBOUNDS)
+            ogSpot = transform.position;
+
+            //If position is top side, glide DOWN to bottom side
+            if (CheckPosition(ogSpot) == 1)
+            {
+                newSpot = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+                
+                Debug.Log("ON TOP SIDE: new spot is: " + newSpot);
+            }
+            //If position is right side, glide LEFT to left side
+            else if (CheckPosition(ogSpot) == 2)
             {
                 newSpot = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
 
-                Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+                Debug.Log("ON RIGHT SIDE: new spot is: " + newSpot);
             }
-            if (transform.position.x != BOARDBOUNDS && transform.position.x != -BOARDBOUNDS)
+            //If position is bottom side, glide UP to top side
+            else if (CheckPosition(ogSpot) == 3)
             {
                 newSpot = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
-                Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
+
+                Debug.Log("ON BOTTOM SIDE: new spot is: " + newSpot);
+            }
+            //If position is left side, glide RIGHT to right side
+            else if (CheckPosition(ogSpot) == 4)
+            {
+                newSpot = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+
+                Debug.Log("ON LEFT SIDE: new spot is: " + newSpot);
+            }
+            //If in the corner, do nothing
+            else if (CheckPosition(ogSpot) == 11 || CheckPosition(ogSpot) == 12
+                  || CheckPosition(ogSpot) == 31 || CheckPosition(ogSpot) == 32)
+            {
+                isGliding = false;
+                Debug.Log("IN CORNER: not gliding");
             }
 
+            //transform.Translate(Vector3.down * Time.deltaTime * speed);
+            transform.position = Vector3.MoveTowards(ogSpot, newSpot, speed * Time.deltaTime);
+
+            if (!isGliding)
+            {
+                transform.position = ogSpot;
+            }
+
+            //Once in position, stop gliding
             if (transform.position == newSpot)
             {
                 isGliding = false;
+                Debug.Log("REACHED POSITION: new position is" + transform.position);
             }
         }
-    }
 
-    void Glide()
-    {
-        Vector3 ogSpot = transform.position;
-        Vector3 newSpot;
-
-        if (transform.position.y != BOARDBOUNDS && transform.position.y != -BOARDBOUNDS)
+        //Starts Attack
+        if (Input.GetKeyDown(KeyCode.LeftShift /*change for arcade*/))
         {
-            newSpot = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            Drop();
+        }
 
-            Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
-        }
-        if (transform.position.x != BOARDBOUNDS && transform.position.x != -BOARDBOUNDS)
-        {
-            newSpot = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
-            Vector3.MoveTowards(ogSpot, newSpot, Time.deltaTime * speed);
-        }
     }
 
     void Drop()
     {
+        Vector3 dropSpot = transform.position;
+        int AOE = 3;
 
+        Vector3 attackTopRight = dropSpot + new Vector3(AOE, AOE);
+        Vector3 attackBottomLeft = dropSpot + new Vector3(-AOE, -AOE);
+
+        Debug.Log("ATTACKED: attacked at spot: " + dropSpot + " AOE is currently" + AOE);
+        Debug.Log("AOE Attack hits area between: Top and Right: " + attackTopRight + ", Bottom and Left: " + attackBottomLeft);
     }
 
     bool IsMovable(Vector3 gridSpot)
@@ -159,4 +190,69 @@ public class PteriMovement : MonoBehaviour
             return false;
         }
     }
+
+    int CheckPosition(Vector3 position)
+    {
+        //Top side = 1 
+        //Right Side = 2
+        //Bottom Side = 3
+        //Left Side = 4
+
+        //Top Side
+        if (transform.position.y == BOARDBOUNDS)
+        {
+            if (transform.position.x == BOARDBOUNDS)
+            {
+                //Top Right Corner
+                return 11;
+            }
+            else if (transform.position.x == -BOARDBOUNDS)
+            {
+                //Top Left Corner
+                return 12;
+            }
+            else
+            {
+                //Top Side (No Corners)
+                return 1;
+            }
+        }
+        //Bottom Side
+        else if (transform.position.y == -BOARDBOUNDS)
+        {
+            if (transform.position.x == BOARDBOUNDS)
+            {
+                //Bottom Right Corner
+                return 31;
+            }
+            else if (transform.position.x == -BOARDBOUNDS)
+            {
+                //Bottom Left Corner
+                return 32;
+            }
+            else
+            {
+                //Bottom side (No Corners)
+                return 3;
+            }
+        }
+        //Right Side
+        else if (transform.position.x == BOARDBOUNDS)
+        {
+            //Right Side (No corners)
+            return 2;
+        }
+        //Left Side
+        else if (transform.position.x == -BOARDBOUNDS)
+        {
+            //Left Side (No corners)
+            return 4;
+        }
+        //not on grid
+        else
+        {
+            return 0;
+        }
+    }
+       
 }
